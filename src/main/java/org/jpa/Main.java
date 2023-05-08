228748889b04a218f760828e8ea556e2cd9eb95d
whdcks420 <whdcks420@gmail.com>
2023-05-08 16:07:16 +0900

    jpa 기본설정 및 테스트

---
+++ src/main/java/org/jpa/Main.java
@@ -0,0 +1,27 @@
+package org.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+
+            Member member = new Member(1L, "bellCold");
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
---
+++ src/main/java/org/jpa/Member.java
@@ -0,0 +1,22 @@
+package org.jpa;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Member {
+
+    @Id
+    private Long id;
+
+    private String name;
+}
